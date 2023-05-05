88bbce3dd15ec24a9375159a30370f4d60b3463f
j-frilot <frilotdr@gmail.com>
2023-05-04 21:07:23 -0500

    overhaulled the timer controls, simplified UI

--- js/app.js
+++ js/app.js
@@ -1,39 +1,41 @@
 const playButton = document.getElementById('play-button');
 const titleSection = document.getElementById('titleSection');
-const gameOneSection = document.getElementById('gameOneSection');
-const elementA = document.getElementById('element-a');
-const elementB = document.getElementById('element-b');
+const gameSection = document.getElementById('gameSection');
+const scoreSection = document.getElementById('scoreSection');
+const endSection = document.getElementById('endSection');
 let answerChoices = document.getElementById('answerChoices');
 let score = 0;
+let time = 20;
 
-//// BEGIN GAME
+//// BEGIN GAME BUTTON EVENT LISTENER
 playButton.addEventListener('click', () => {
 	titleSection.classList.add('d-none');
-	gameOneSection.classList.remove('d-none');
-	const randomNumA = Math.floor(Math.random() * 12);
-	const randomNumB = Math.floor(Math.random() * 12);
-	hideStartScreen();
-	gameOne(randomNumA, randomNumB);
+	gameSection.classList.remove('d-none');
+	scoreSection.classList.remove('d-none');
+	generatePrablem();
+	startCountdown(time);
 });
 
-function hideStartScreen() {
-	console.log('hideStartScreen Function');
+//// GENERATE AN RANDOM EQUATION
+function generatePrablem() {
+	const randomNumA = Math.floor(Math.random() * 12);
+	const randomNumB = Math.floor(Math.random() * 12);
+	document.getElementById('element-a').innerHTML = randomNumA;
+	document.getElementById('element-b').innerHTML = randomNumB;
+	const correctAnswer = randomNumA + randomNumB;
+	console.log(`${randomNumA} + ${randomNumB} = ${correctAnswer}`);
+	generateAnswerChoices(correctAnswer);
 }
 
-//// GAME 1
-function gameOne(randomNumA, randomNumB) {
-	elementA.innerHTML = randomNumA;
-	elementB.innerHTML = randomNumB;
-	const correctGameOneAnswer = randomNumA + randomNumB;
-	console.log(`${randomNumA} + ${randomNumB} = ${correctGameOneAnswer}`);
-
+//// GENERATE RANDOM ANSWER CHOICES
+function generateAnswerChoices(correctAnswer) {
 	//come up with random choices & check that choices are not same as correct answer
 	let answerChoiceArray = [];
 	for (let i = 0; i < 3; i++) {
 		answerChoiceArray.push(Math.floor(Math.random() * 24));
 
 		for (let j = 0; j <= answerChoiceArray.length; j++) {
-			if (answerChoiceArray[i] === correctGameOneAnswer) {
+			if (answerChoiceArray[i] === correctAnswer) {
 				answerChoiceArray[i] * Math.floor(Math.random() * 2);
 			}
 		}
@@ -42,14 +44,68 @@ function gameOne(randomNumA, randomNumB) {
 	answerChoiceArray.splice(
 		Math.floor(Math.random() * answerChoiceArray.length + 1),
 		0,
-		correctGameOneAnswer
+		correctAnswer
 	);
 	//add choices to DOM
 	const addChoices = answerChoiceArray
 		.map((answers) => {
 			return `
-            <button class="btn btn-primary">${answers}</button>`;
+            <button id="answerChoiceBtns" class="btn btn-primary">${answers}</button>`;
 		})
 		.join('');
 	answerChoices.innerHTML = addChoices;
+	console.log('Correct Answer:', correctAnswer);
+	selectAnswer(correctAnswer);
+}
+
+//// SELECT ANSWER
+function selectAnswer(correctAnswer) {
+	const answerChoiceBtns = document.querySelectorAll('#answerChoiceBtns');
+	answerChoiceBtns.forEach((btn) => {
+		btn.addEventListener('click', (e) => {
+			if (Number(e.target.innerHTML) === correctAnswer) {
+				score++;
+				console.log('Target:', Number(e.target.innerHTML));
+				changeScore();
+				generatePrablem();
+			} else {
+				console.log('wrong');
+				score--;
+				changeScore();
+			}
+		});
+	});
+}
+
+//// CHANGE SCORE
+function changeScore() {
+	const currentScore = document.getElementById('currentScore');
+	currentScore.innerHTML = score;
+}
+
+//// TIMER CONTROLS
+function startCountdown() {
+	const timer = document.getElementById('timer');
+	timer.innerHTML = time;
+
+	const interval = setInterval(() => {
+		time--;
+		timer.innerHTML = time;
+		console.log(time);
+
+		if (time === 0) {
+			clearInterval(interval);
+			console.log('times up');
+			finishGame();
+		}
+	}, 1000);
+}
+
+//// FINISH GAME
+function finishGame() {
+	gameSection.classList.add('d-none');
+	scoreSection.classList.add('d-none');
+	endSection.classList.remove('d-none');
+
+	document.getElementById('finalPoints').innerHTML = score;
 }
